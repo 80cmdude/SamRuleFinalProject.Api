e285f7e7467e4373db3379bf1c90491353618e33
Samuel Rule <samuel.rule@reedonline.co.uk>
2017-09-20 14:36:19 +0100

    Added sign in method.

--- FinalProjectApi/FinalProjectApi/Controllers/SignInController.cs
+++ FinalProjectApi/FinalProjectApi/Controllers/SignInController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FinalProjectApi.Models;
 using FinalProjectApi.Helpers;
+using Newtonsoft.Json;
 
 namespace FinalProjectApi.Controllers
 {
@@ -15,9 +16,47 @@ namespace FinalProjectApi.Controllers
 		// POST: api/SignIn
 		[Route("api/SignIn")]
 		[HttpPost]
-		public void Post([FromBody]User value)
+		public string Post([FromBody]User user)
 		{
-			string cake = "d";
+			object message;
+			string response;
+			string queryCheckUser = "SELECT * FROM USER WHERE EmployeeNumber = ?";
+
+			try
+			{
+				var existingUser = Program.UserDatabase.Query<User>(queryCheckUser, new string[] { $"{user.EmployeeNumber}" });
+
+				if (existingUser.Count == 0)
+					return ResponseHelpers.FailureResponse("User does not exist");
+
+				if (existingUser[0].Password == user.Password)
+				{
+					existingUser[0].Token = TokenHelper.GenerateToken(user.FirstName, user.LastName);
+
+					var success = Program.UserDatabase.SaveItem(existingUser[0]);
+					if (success != 0)
+					{
+						message = new
+						{
+							success = true,
+							Id = existingUser[0].ID,
+							Token = existingUser[0].Token,
+						};
+						response = JsonConvert.SerializeObject(message);
+						return response;
+					}
+					else
+					{
+						return ResponseHelpers.FailureResponse("Failed adding user to database");
+					}
+				}
+				return "Invalid Password";
+				
+			}
+			catch (Exception e)
+			{
+				return ResponseHelpers.FailureResponse("Information sent was not correct");
+			}
 		}
 
 		[Route("api/SignOut")]
