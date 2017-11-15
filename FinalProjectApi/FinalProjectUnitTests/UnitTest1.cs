5484f5eac21b21fd164a02aba5eb430c99efd5e3
Samuel Rule <samuel.rule@reedonline.co.uk>
2017-11-15 12:54:05 +0000

    Added unit test project

---
+++ FinalProjectApi/FinalProjectUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FinalProjectUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
